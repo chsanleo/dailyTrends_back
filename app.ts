8bd9174556d300fa5a28858415c7ec1c90e7578f
Christian <chsanleo@gmail.com>
2021-09-04 20:49:34 +0200

    use the value of config import or enviroment

--- app.ts
+++ app.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import config from './config/config.js';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || config.PORT;
 
 app.listen(port, () => {
     console.log(`Running on port ${port}.`);
