31b63055acb453c91f62b10409f70547b85d2158
Christian <chsanleo@gmail.com>
2021-09-04 12:06:07 +0200

    Change message

--- app.ts
+++ app.ts
@@ -4,5 +4,5 @@ const app = express();
 const port = 3000;
 
 app.listen(port, () => {
-  console.log(`Timezones by location application is running on port ${port}.`);
+    console.log(`Running on port ${port}.`);
 });
