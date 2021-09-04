2a347e78ecef6b1a691987597589312914e99ea6
Christian <chsanleo@gmail.com>
2021-09-04 20:49:55 +0200

    import config file and iniciate connection to mongoDB

--- app.ts
+++ app.ts
@@ -4,6 +4,11 @@ import config from './config/config.js';
 const app = express();
 const port = process.env.PORT || config.PORT;
 
+//#region MongoConectionDB
+const dbconnect = require('./core/database/config/config.ts');
+dbconnect();
+//#endregion
+
 app.listen(port, () => {
     console.log(`Running on port ${port}.`);
 });
