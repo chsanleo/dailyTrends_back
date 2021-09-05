463442365ce09e6bd9284eb14fd05fc0fc918892
Christian <chsanleo@gmail.com>
2021-09-05 23:57:32 +0200

    improve legibility of method

--- core/utils/utils.ts
+++ core/utils/utils.ts
@@ -1,1 +1,3 @@
-export const dateNowSQL = (): string => { return new Date().toISOString().slice(0, 19).replace('T', ' '); };
+export const dateNowSQL = (): string => {
+    return new Date().toISOString().slice(0, 19).replace('T', ' ');
+};
