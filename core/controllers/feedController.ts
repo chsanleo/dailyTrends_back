4935ab7729e20abf2fd7ffd5f565ba8977dd62ae
Christian <chsanleo@gmail.com>
2021-09-06 00:05:07 +0200

    create method get in feedController

--- core/controllers/feedController.ts
+++ core/controllers/feedController.ts
@@ -2,3 +2,12 @@ import { Request, Response } from 'express';
 
 import { Feed } from '../types/Feed';
 import * as feedService from '../services/feedService';
+
+export const get = async (req: Request, res: Response) => {
+    try {
+        let feed: Feed = await feedService.get(req.params.id);
+        res.status(200).send(feed);
+    } catch (e) {
+        res.status(500).send(e.message);
+    }
+};
