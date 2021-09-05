162544923a155606f048064323c028ac8944abde
Christian <chsanleo@gmail.com>
2021-09-06 00:05:59 +0200

    add in feedRouter the path to methods in feedController

--- routers/feedRouter.ts
+++ routers/feedRouter.ts
@@ -2,3 +2,7 @@ import express from 'express';
 export const feedRouter = express.Router();
 
 import * as feedController from '../core/controllers/feedController';
+
+feedRouter.get('/:id', feedController.get);
+feedRouter.get('/', feedController.getAll);
+feedRouter.get('/db', feedController.getAllDB);
