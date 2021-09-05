6e9d123777235cc90f9320b6fd4676b45724ce5f
Christian <chsanleo@gmail.com>
2021-09-06 00:52:57 +0200

    change position app constant

--- app.ts
+++ app.ts
@@ -8,17 +8,15 @@ dotenv.config();
 if (!process.env.PORT) { process.exit(1); }
 let port: number = parseInt(process.env.PORT as string, 10);;
 
+const app = express();
 
 //#region MongoConectionDB
 const dbconnect = require('./core/database/config/config.ts');
 dbconnect();
 //#endregion
 
-const app = express();
-
 app.use(cors());
 app.use(express.json());
-
 
 //#region router
 import { manualImportRouter } from './routers/manualInputRouter';
