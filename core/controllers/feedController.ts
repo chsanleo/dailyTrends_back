484bee27d40083af05c2930112d461e7e5af994b
Christian <chsanleo@gmail.com>
2021-09-06 00:05:30 +0200

    create method getAll in controller

--- core/controllers/feedController.ts
+++ core/controllers/feedController.ts
@@ -11,3 +11,11 @@ export const get = async (req: Request, res: Response) => {
         res.status(500).send(e.message);
     }
 };
+export const getAll = async(req: Request, res: Response)=>{
+    try {
+        let feedList: Feed [] = await feedService.getAll();
+        res.status(200).send(feedList);
+    } catch (e) {
+        res.status(500).send(e.message);
+    }
+};
