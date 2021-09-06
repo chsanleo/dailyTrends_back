cbbff63307a1d6ba8cca791cd570e1b40142a9be
Christian <chsanleo@gmail.com>
2021-09-06 13:52:02 +0200

    create and init with imports scraping file to PAIS newspaper

---
+++ core/services/dataScrapingPAISService.ts
@@ -0,0 +1,7 @@
+import axios from 'axios';
+import Cheerio from 'cheerio';
+
+import * as utils from '../utils/utils';
+
+import { Feed } from '../types/Feed';
+import { logger } from '../../config/config_log';
