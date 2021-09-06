a494b56aa84459391e5ace399f36ac2d1a9cef6a
Christian <chsanleo@gmail.com>
2021-09-06 13:56:14 +0200

    implemets method to create objects with the information need

--- core/services/dataScrapingPAISService.ts
+++ core/services/dataScrapingPAISService.ts
@@ -10,6 +10,31 @@ const url = "https://elpais.com";
 
 export const scrapeData = async (): Promise<Feed[]> => {
     try {
+
+        const { data } = await axios.get(url);
+
+        let feedList: Feed[] = [];
+        let $ = Cheerio.load(data);
+        let listItems = $(".headline");
+
+        listItems.each((idx, el) => {
+            let link: string = ($(el).children("a").attr("href"));
+
+            let fullDate: string = utils.dateNowSQL();
+            let today: string = fullDate.substring(0, 10);
+
+            if (link.includes(today)) {
+                let feed: Feed = {
+                    title: $(el).children("a").text(),
+                    dateCreated: fullDate,
+                    link: url + link,
+                    originUrl: url,
+                    origin: 'auto'
+                }
+                feedList.push(feed);
+            }
+        });
+        return feedList;
     } catch (err) {
         logger.error(err);
     }
