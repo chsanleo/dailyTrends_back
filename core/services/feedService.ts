64ae776900e256f35b0f919239522dade044e783
Christian <chsanleo@gmail.com>
2021-09-06 13:54:36 +0200

    import scrapingPais Service

--- core/services/feedService.ts
+++ core/services/feedService.ts
@@ -3,6 +3,7 @@ import * as utils from '../utils/utils';
 import * as validations from '../utils/validations';
 import { Feed } from '../types/Feed';
 
+import * as dataScrapingPAISService from './dataScrapingPAISService';
 export const create = async (newFeed: Feed): Promise<Feed> => {
     let feed: Feed = newFeed;
 
