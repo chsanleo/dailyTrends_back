b96fb52ee1f6483d450cc8870702dd1be5e3c6f1
Christian <chsanleo@gmail.com>
2021-09-06 14:40:08 +0200

    create file to scraping in Mundo newspapers and init imports

---
+++ core/services/dataScrapingMUNDOService.ts
@@ -0,0 +1,9 @@
+import axios from 'axios';
+import Cheerio from 'cheerio';
+
+import * as utils from '../utils/utils';
+
+import { Feed } from '../types/Feed';
+import { logger } from '../../config/config_log';
+
+const url = "https://www.elmundo.es";
