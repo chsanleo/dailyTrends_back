1b6f3e6c653ba1c86eb241d84ee7372eb485d8ff
Christian <chsanleo@gmail.com>
2021-09-06 14:43:59 +0200

    implemets method scraping mundo.es

--- core/services/dataScrapingMUNDOService.ts
+++ core/services/dataScrapingMUNDOService.ts
@@ -10,7 +10,31 @@ const url = "https://www.elmundo.es";
 
 export const scrapeData = async (): Promise<Feed[]> => {
     try {
+
+        const { data } = await axios.get(url);
+
         let feedList: Feed[] = [];
+        let $ = Cheerio.load(data);
+        let listItems = $(".ue-c-cover-content__link");
+
+        listItems.each((idx, el) => {
+            
+            let link: string = ($(el).attr("href"));
+
+            let fullDate: string = utils.dateNowSQL();
+            let today: string = fullDate.substring(0, 10).replace(/-/g,'/');
+
+            if (link.includes(today)) {
+                let feed: Feed = {
+                    title: $(el).text(),
+                    dateCreated: fullDate,
+                    link: link,
+                    originUrl: url,
+                    origin: 'auto'
+                }
+                feedList.push(feed);
+            }
+        });
         return feedList;
     } catch (err) {
         logger.error(err);
